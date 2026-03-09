080d3be009331bee404bdb33a50303c1d8af105b
y9828 <y9828@naver.com>
2026-03-09 18:22:46 +0900

    [생성] STS 스프링 부트와 API 서버 생성

---
+++ mallapi/src/main/java/com/kh/mallapi/config/RootConfig.java
@@ -0,0 +1,19 @@
+package com.kh.mallapi.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+	@Bean
+	public ModelMapper getMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setFieldMatchingEnabled(true) // 이름이일치하는경우 매핑
+				.setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE) // 필드접근수준 private
+				.setMatchingStrategy(MatchingStrategies.LOOSE); // 필드이름이 비슷하면 매핑 (저사년=>저상현)
+		return modelMapper;
+	}
+
+}
